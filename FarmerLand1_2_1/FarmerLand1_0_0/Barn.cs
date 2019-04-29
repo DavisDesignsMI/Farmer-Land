fc2537d669086f7048320aca9d602cbd5faf0c95
DavisDesignsMI <49576769+DavisDesignsMI@users.noreply.github.com>
2019-04-29 18:27:20 -0400

    Add files via upload
    Second release of 1.2.0 due to error where cash was too high (I forgot to change it back when I was done testing).

---
+++ FarmerLand1_2_1/FarmerLand1_0_0/Barn.cs
@@ -0,0 +1,71 @@
+﻿/* CREATED BY: Davis M. Brace  (Davis Designs of Michigan LLC)
+ * STARTED ON: 04/25/2019
+ * TITLE: Farmerland Barn Extension
+ * 
+ * DESCRIPTION:
+ * Class of the FarmerLand app.  Represents a barn for animals and associated data
+ * and methods.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace FarmerLand
+{
+    [Serializable]
+    class Barn
+    {
+        public List<int> owned = new List<int>();
+
+        public bool TryBuy(int selection, double cash)
+        {
+            if (!owned.Contains(selection))
+            {
+                //Calculate cost
+                double cost = selection * 10000;
+
+                //Determine if cost is too high
+                if (cost <= cash)
+                {
+                    return true;
+                }
+            }
+
+            //Return 0 as default
+            return false;
+        }
+
+        public double Buy(int selection, double cash)
+        {
+            if (!owned.Contains(selection))
+            {
+                //Calculate cost
+                double cost = selection * 10000;
+
+                //Determine if cost is too high
+                if (cost <= cash)
+                {
+                    //Purchase
+                    owned.Add(selection);
+                    return cost;
+                }
+            }
+
+            //Return 0 as default
+            return 0;
+        }
+
+        public double Collect()
+        {
+            //Initiate revenue variable
+            double revenue = 0;
+
+            foreach (int animal in owned)
+            {
+                revenue += animal * 2000;
+            }
+
+            return revenue;
+        }
+    }
+}
---
+++ FarmerLand1_2_1/FarmerLand1_0_0/Expand.cs
@@ -0,0 +1,90 @@
+﻿/* CREATED BY: Davis M. Brace  (Davis Designs of Michigan LLC)
+ * STARTED ON: 04/25/2019
+ * TITLE: Farmer Land
+ * 
+ * DESCRIPTION:
+ * This is an extension of Farmerland which allows the user to input new dimensions for a field.
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace FarmerLand
+{
+    public partial class Expand : Form
+    {
+        //Initiate double vars
+        public double X;
+        public double Y;
+        public double Cost;
+        public double Cash;
+
+        //Initiate bool vars
+        public bool complete = false;
+        public bool cancel = false;
+
+        public Expand(double cash)
+        {
+            Cash = cash;
+            InitializeComponent();
+            TickUpdate();
+        }
+
+        private void Expand_Load(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+
+        private void Accept_Click(object sender, EventArgs e)
+        {
+            TickUpdate();
+            if (Cost < Cash)
+            {
+                complete = true;
+            }
+            else
+            {
+                MessageBox.Show("This expansion is too expensive.");
+            }
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            TickUpdate();
+            complete = true;
+            cancel = true;
+        }
+
+        private void Xin_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                X = double.Parse(Xin.Text);
+            }
+            catch
+            {
+                Xin.Text = X.ToString();
+            }
+
+            TickUpdate();
+        }
+
+        private void Yin_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Y = double.Parse(Yin.Text);
+            }
+            catch
+            {
+                Yin.Text = Y.ToString();
+            }
+
+            TickUpdate();
+        }
+
+        public void TickUpdate()
+        {
+            Cost = (X * Y * 20) - 2000;
+            CostOut.Text = "Cost: " + Cost.ToString();
+        }
+    }
+}
